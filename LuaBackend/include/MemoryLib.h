8d4a9d99a6e559161a89fd936b7e283a06ec6c77
TopazTK <topaz.maxwilliam@gmail.com>
2021-02-13 13:55:15 +0300

    Add mIni, add config logic, fix some bugs, implement support for Big Endian.

--- LuaBackend/include/MemoryLib.h
+++ LuaBackend/include/MemoryLib.h
@@ -12,13 +12,18 @@ class MemoryLib
     public:
     static inline STARTUPINFOA _sInfo;
     static inline PROCESS_INFORMATION _pInfo;
+    static inline uint32_t _baseAddress;
+    static inline bool _bigEndian;
 
-    static void ExecuteProcess(string InputName)
+    static void ExecuteProcess(string InputName, uint32_t InputAddress, bool InputEndian)
     {
         ZeroMemory(&_sInfo, sizeof(_sInfo)); _sInfo.cb = sizeof(_sInfo);
         ZeroMemory(&_pInfo, sizeof(_pInfo));
 
         CreateProcessA(InputName.c_str(), NULL, NULL, NULL, TRUE, 0, NULL, NULL, &_sInfo, &_pInfo);
+
+        _baseAddress = InputAddress;
+        _bigEndian = InputEndian;
     };
 
     static uint8_t ReadByte(int Address) { return ReadBytes(Address, 1)[0]; }
@@ -27,18 +32,26 @@ class MemoryLib
         vector<uint8_t> _buffer;
         _buffer.resize(Length);
 
-        ReadProcessMemory(_pInfo.hProcess, (void*)Address, _buffer.data(), Length, 0);
+        ReadProcessMemory(_pInfo.hProcess, (void*)(Address + _baseAddress), _buffer.data(), Length, 0);
         return _buffer;
     }
     static uint16_t ReadShort(int Address)
     {
         auto _buffer = ReadBytes(Address, 2);
-        return (_buffer[1] << 8) | _buffer[2];
+
+        if (_bigEndian)
+            return (_buffer[0] << 8) | _buffer[1];
+        else
+            return (_buffer[1] << 8) | _buffer[0];
     }
     static uint32_t ReadInt(int Address)
     {
         auto _buffer = ReadBytes(Address, 4);
-        return (_buffer[3] << 24) | (_buffer[2] << 16) | (_buffer[1] << 8) | (_buffer[0]);
+
+        if (_bigEndian)
+            return (_buffer[0] << 24) | (_buffer[1] << 16) | (_buffer[2] << 8) | (_buffer[3]);
+        else
+            return (_buffer[3] << 24) | (_buffer[2] << 16) | (_buffer[1] << 8) | (_buffer[0]);
     }
     static float ReadFloat(int Address)
     {
@@ -58,14 +71,20 @@ class MemoryLib
         return _output;
     }
 
-    static void WriteByte(int Address, uint8_t Input) { WriteProcessMemory(_pInfo.hProcess, (void*)Address, &Input, 1, 0); }
-    static void WriteBytes(int Address, vector<uint8_t> Input) { WriteProcessMemory(_pInfo.hProcess, (void*)Address, Input.data(), Input.size(), 0); }
+    static void WriteByte(int Address, uint8_t Input) { WriteProcessMemory(_pInfo.hProcess, (void*)(Address + _baseAddress), &Input, 1, 0); }
+    static void WriteBytes(int Address, vector<uint8_t> Input) { WriteProcessMemory(_pInfo.hProcess, (void*)(Address + _baseAddress), Input.data(), Input.size(), 0); }
     static void WriteShort(int Address, uint16_t Input)
     {
         vector<uint8_t> _write(2);
         
         for (int i = 0; i < 2; i++)
-            _write[1 - i] = (Input >> (i * 8)) & 0xFF;
+        {
+            if (_bigEndian)
+                _write[1 - i] = (Input >> (i * 8)) & 0xFF;
+
+            else
+                _write[i] = (Input >> (i * 8)) & 0xFF;
+        }
 
         WriteBytes(Address, _write);
         _write.clear();
@@ -75,7 +94,13 @@ class MemoryLib
         vector<uint8_t> _write(4);
 
         for (int i = 0; i < 4; i++)
-            _write[3 - i] = (Input >> (i * 8)) & 0xFF;
+        {
+            if (_bigEndian)
+                _write[3 - i] = (Input >> (i * 8)) & 0xFF;
+
+            else
+                _write[i] = (Input >> (i * 8)) & 0xFF;
+        }
 
         WriteBytes(Address, _write);
         _write.clear();
@@ -92,5 +117,4 @@ class MemoryLib
         WriteBytes(Address, _value);
     }
 };
-
 #endif
--- LuaBackend/source/main.cpp
+++ LuaBackend/source/main.cpp
@@ -1,19 +1,21 @@
 #include <thread>
+#include <future>
 #include <iostream>
 #include <windows.h>
 #include <filesystem>
-#include <MemoryLib.h>
 
+#include <MemoryLib.h>
 #include <LuaBackend.h>
-#include <future>
+
+#include <mIni/ini.h>
 
 using namespace std;
-
+using namespace mINI;
 LuaBackend* _backend = nullptr;
 
-void _execute(std::future<void> futureObj)
+void _execute(future<void> futureObj)
 {
-	while (futureObj.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout)
+	while (futureObj.wait_for(chrono::milliseconds(1)) == future_status::timeout)
 	{
 		for (auto _script : _backend->loadedScripts)
 			if (_script->frameFunction)
@@ -23,39 +25,89 @@ void _execute(std::future<void> futureObj)
 
 int main()
 {
-	cout << "=== LuaBackend | v0.01 ALPHA ===" << "\n";
 	cout << "================================" << "\n";
+	cout << "=== LuaBackend | v0.01 ALPHA ===" << "\n";
+	cout << "=== Copyright - TopazTK 2021 ===" << "\n";
+	cout << "================================" << "\n\n";
 
-	std::string _currPath = std::filesystem::current_path().u8string();
-	_currPath.append("\\emu\\pcsx2.exe");
+	string _currPath = filesystem::current_path().u8string();
+	string _confPath = _currPath;
+	string _scriptPath = _currPath;
 
-	cout << "MESSAGE: Found linked executable! Running..." << "\n";
-	MemoryLib::ExecuteProcess(_currPath);
+	_confPath.append("\\config.ini");
+	_scriptPath.append("\\scripts\\");
 
-	cout << "MESSAGE: Hooking and initializing the backend..." << "\n";
-	_backend = new LuaBackend();
+	INIFile _iniFile(_confPath);
+	INIStructure _iniStruct;
 
-	cout << "MESSAGE: Executing initialization event handlers..." << "\n\n";
-	for (auto _script : _backend->loadedScripts)
-		if (_script->initFunction)
-			_script->initFunction();
-	
-	std::promise<void> exitSignal;
-	std::future<void> futureObj = exitSignal.get_future();
-	std::thread _frameThread(&_execute, std::move(futureObj));
+	bool _readSuccess = _iniFile.read(_iniStruct);
 
-	while (true)
+
+	if (_readSuccess)
 	{
-		string _output;
-		getline(std::cin, _output);
-		std::transform(_output.begin(), _output.end(), _output.begin(), ::tolower);
-
-		if (_output == "exit")
+		if (_iniStruct.has("CONFIG"))
 		{
-			exitSignal.set_value();
-			_frameThread.join();
-			break;
+			auto _table = _iniStruct["CONFIG"];
+
+			if (_table.has("EXECUTABLE"))
+			{
+				auto _exec = _table["EXECUTABLE"];
+				auto _addr = _table["BASE_ADDRESS"];
+
+				bool _bigEndian = false;
+
+				if (_table.has("BIG-ENDIAN"))
+				{
+					string _boolGet = _table["BIG-ENDIAN"];
+					transform(_boolGet.begin(), _boolGet.end(), _boolGet.begin(), ::tolower);
+
+					_bigEndian = _boolGet == "true" ? true : false;
+				}
+
+				_currPath.append("\\" + _exec);
+				uint32_t _baseAddress = stoi(_addr.c_str(), nullptr, 16);
+
+				cout << "MESSAGE: Found linked executable! Running..." << "\n";
+				MemoryLib::ExecuteProcess(_currPath, _baseAddress, _bigEndian);
+				
+				if (!filesystem::exists(_scriptPath))
+					filesystem::create_directory(_scriptPath);
+
+				cout << "MESSAGE: Hooking and initializing the backend..." << "\n";
+				_backend = new LuaBackend();
+
+				cout << "\n" << "MESSAGE: Executing initialization event handlers..." << "\n\n";
+				for (auto _script : _backend->loadedScripts)
+					if (_script->initFunction)
+						_script->initFunction();
+
+				promise<void> exitSignal;
+				future<void> futureObj = exitSignal.get_future();
+				std::thread _frameThread(&_execute, std::move(futureObj));
+
+				while (true)
+				{
+					string _output;
+					getline(std::cin, _output);
+					std::transform(_output.begin(), _output.end(), _output.begin(), ::tolower);
+
+					if (_output == "exit")
+					{
+						exitSignal.set_value();
+						_frameThread.join();
+						break;
+					}
+				}
+			}
+
+			else
+				cout << "ERROR: Invalid config! Field \"EXECUTABLE\" not found!" << "\n";
 		}
+
+		else
+			cout << "ERROR: Invalid config! Table \"CONFIG\" not found!" << "\n";
 	}
 
+	else
+		cout << "ERROR: Config file \"config.ini\" not found!" << "\n";
 }
